9acf8603ebb0afd14b8781e853b13f15d9fce207
nickibdish <nickibdish@yandex.ru>
2023-11-02 16:47:05 +0300

    не работает напоминание в тг

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
+import telebot
 import schedule
 import time
-import datetime
 
-# Словарь с датами и именами
-dates_and_names = {
-    '2023-11-02': 'John',
-    '2023-12-25': 'Alice',
-    '2023-10-03': 'Bob'
+# Замените 'YOUR_BOT_TOKEN' на токен вашего бота
+bot = telebot.TeleBot('6568976463:AAG7UZSuZYkiJtsFBOCKKORuu3BztgElW1k')
+
+# Создаем словарь с днями рождения
+birthdays = {
+    '2023-11-02': 'user1',
+    '2023-08-06': 'user2',
+    # Добавьте сюда дни рождения в формате 'гггг-мм-дд': 'Имя пользователя'
 }
 
-def send_birthday_greetings():
-    current_date = datetime.date.today()
-    current_date_str = current_date.strftime('%Y-%m-%d')
+# Функция для отправки сообщения с поздравлением
+def send_birthday_greeting(chat_id, user_name):
+    greeting = f'C днем рождения, {user_name}!'
+    bot.send_message(chat_id, greeting)
 
-    if current_date_str in dates_and_names:
-        name = dates_and_names[current_date_str]
-        print(f"Сегодня именинник: {name}")
-        # Здесь можно добавить код для отправки поздравления (например, через Telegram бота)
+# Функция, которая проверяет, есть ли день рождения сегодня
+def check_birthdays():
+    today = time.strftime('%Y-%m-%d')
+    if today in birthdays:
+        for chat_id, user_name in birthdays[today]:
+            send_birthday_greeting(chat_id, user_name)
 
-# Создаем задачу, которая будет выполняться каждый день в полночь
-schedule.every().day.at("11:33").do(send_birthday_greetings)
+# Расписание для проверки дней рождения каждый день в полночь
+schedule.every().day.at('12:00').do(check_birthdays)
+
+# Обработчик команды /start
+@bot.message_handler(commands=['start'])
+def start(message):
+    chat_id = message.chat.id
+    print(chat_id)
+    user_name = message.from_user.first_name  # Получаем имя пользователя
+    bot.send_message(chat_id, f'Привет, {user_name}! я буду напоминать тебе, когда у твоих друзей день рождения!')
+
 
 while True:
     schedule.run_pending()
-    time.sleep(1)
+    bot.polling(none_stop=True)
